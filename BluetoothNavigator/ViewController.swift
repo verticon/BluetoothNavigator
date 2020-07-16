7aeae2da1328fc33254a25146e982c6c44ed270c
Robert Vaessen <robert@rvaessen.com>
2020-07-16 11:04:39 -0400

    Initial Commit

---
+++ BluetoothNavigator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BluetoothNavigator
+//
+//  Created by Robert Vaessen on 7/16/20.
+//  Copyright © 2020 Robert Vaessen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
