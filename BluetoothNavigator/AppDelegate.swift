c515c922753aa6b828f5942f1c3582d69b8a0af5
Robert Vaessen <robert@rvaessen.com>
2020-07-16 19:56:30 -0400

    Converted to swift 5

--- BluetoothNavigator/AppDelegate.swift
+++ BluetoothNavigator/AppDelegate.swift
@@ -1,37 +1,75 @@
-//
-//  AppDelegate.swift
-//  BluetoothNavigator
-//
-//  Created by Robert Vaessen on 7/16/20.
-//  Copyright © 2020 Robert Vaessen. All rights reserved.
-//
 
 import UIKit
+//import UserNotifications
+import VerticonsToolbox
+import MoBetterBluetooth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    fileprivate let notifier = BeaconNotifier()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        func printStartupMessage() {
+            var message = "\(applicationName) was launched \(LocalTime.text) "
+            if let options = launchOptions {
+                message += "with options:"
+                for (key, value) in options { message += "\n\t\(key) = \(value)" }
+            }
+            else {
+                message += "without options."
+            }
+            print(message)
+        }
+
+
+        func setupWindow() {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window!.makeKeyAndVisible()
+            window!.backgroundColor = .black
+
+            window!.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+            //window!.rootViewController = StartupController()
+        }
+        
+        // *******************************************************************
+        
+        printStartupMessage()
+
+        BluetoothNavigatorTheme.configure()
+
+        if let _ = launchOptions?[UIApplication.LaunchOptionsKey.location] { _ = BeaconRegion.restore() }
+
+        setupWindow()
+
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        print("\(applicationName) will become inactive")
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("\(applicationName) moved to the background")
     }
 
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        print("\(applicationName) will move to the foreground")
+    }
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("\(applicationName) became active")
+    }
 
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("\(applicationName) will terminate")
+    }
 }
-
